f4a05b882b4f487e01d5b085f0044aeffa700513
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2025-04-10 18:04:47 +0200

    Add debug logging and args

--- src/template/template.py
+++ src/template/template.py
@@ -5,6 +5,7 @@ Baseline template for future Python code related to this project.
 Replace this docstring and code below with your own code as required.
 """
 
+import argparse
 import logging
 import time
 
@@ -28,9 +29,23 @@ logger = logging.getLogger(__name__)
 def main() -> None:
     """Primary entry point for this script."""
 
-    logger.info(
-        "hello world!"
-    )  # logging is lowercase, and in other cases should be informative.
+    parser = argparse.ArgumentParser(
+        prog="json-vorhids",
+        description="proof-of-concept identifier for JSON objects on disk based on identifying valid objects and their key-values",
+        epilog="for more information visit https://github.com/ffdev-info/json-vorhIDS",
+    )
+    parser.add_argument(
+        "--debug",
+        help="use debug loggng",
+        required=False,
+        action="store_true",
+    )
+
+    args = parser.parse_args()
+    logging.getLogger(__name__).setLevel(
+        logging.DEBUG if args.debug else logging.WARNING
+    )
+    logger.debug("debug logging is configured")
 
 
 if __name__ == "__main__":
