1d464f91226a9fd440700e8b2278f6d02b951c71
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2025-10-22 20:44:14 +0200

    WIP: lookup

---
+++ src/jsonid/lookup.py
@@ -0,0 +1,5 @@
+"""Provide lookup functions for the registry."""
+
+# TODO: enable registry lookup by ID/Reference.
+# TODO: base entries not in the registry, i.e. TOML/JSON/YAML/JSONL
+#       base formats must also be included.
