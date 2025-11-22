b8ae47b224225839ccde64d8240384c3e00dcf38
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2025-11-22 17:32:29 +0100

    WIP; fix encoding utils add new

---
+++ json2encoding_util.py
@@ -0,0 +1,14 @@
+"""json2json will convert JSON compatible objects from one encoding
+to UTF-8.
+"""
+
+from src.utils import json2encoding
+
+
+def main():
+    """Primary entry point for this script."""
+    json2encoding.main()
+
+
+if __name__ == "__main__":
+    main()
